b0a9577687af780ce1c000fd7b92e68288942bce
Miller <guf302010@gmail.com>
2021-09-23 20:47:44 +0700

    Initial Commit

---
+++ ContactsApp/ContactsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsAppApp.swift
+//  ContactsApp
+//
+//  Created by Robert Miller on 23.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct ContactsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
